726f491ff0b27718f6bc52307e81c2f866cbdbd9
DanRivalle <dan.santos.hd@outlook.com>
2023-04-05 23:13:05 -0400

    Se deja listo para trabajar tst.py

---
+++ tst.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import utility as ut
+
+def save_measure(cm, Fsc):
+  pass
+
+
+def load_w():
+  pass
+
+
+def load_data_test():
+  pass
+    
+
+# Beginning ...
+def main():			
+  xv, yv  = load_data_test()
+  W = load_w()
+  zv = ut.forward(xv, W)      		
+  cm, Fsc = ut.metricas(yv, zv) 	
+  save_measure(cm, Fsc)
+		
+
+if __name__ == '__main__':   
+	 main()
+
