af18ecc3273f0ce4a237559ada5423627cdd2b33
DanRivalle <dan.santos.hd@outlook.com>
2023-04-05 23:05:27 -0400

    Se deja listo para trabajar utility

---
+++ utility.py
@@ -0,0 +1,56 @@
+# My Utility : auxiliars functions
+
+import pandas as pd
+import numpy  as np
+  
+#load parameters to train the SNN
+def load_cnf():
+  pass
+
+
+# Initialize weights for SNN-SGDM
+def iniWs(Param):    
+  pass
+
+
+# Initialize weights for one-layer    
+def iniW(next, prev):
+  r = np.sqrt(6 / (next + prev))
+  w = np.random.rand(next, prev)
+  w = w * 2 * r - r    
+  return w
+
+# Feed-forward of SNN
+def forward(...):
+  pass
+
+
+#Activation function
+def act_function(...):
+  pass
+
+
+# Derivatives of the activation funciton
+def deriva_act(...):
+  pass
+
+
+#Feed-Backward of SNN
+def gradW(...):    
+  pass
+
+
+# Update W and V
+def updWV_sgdm(...):
+  pass
+
+
+# Measure
+def metricas(x,y):
+  pass
+    
+
+#Confusion matrix
+def confusion_matrix(z,y):
+  pass
+
