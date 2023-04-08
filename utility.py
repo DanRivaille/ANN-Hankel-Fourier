379325e98e5c49570aacc22ac357dc20e8088720
Patricio Labra <patricio.labra827@gmail.com>
2023-04-07 20:59:53 -0400

    se cambia xv por x, se agrega la funcion sigmoide a la ultima iteración

--- utility.py
+++ utility.py
@@ -79,18 +79,19 @@ def create_ann(hidden_nodes, X):
 
 
 # Feed-forward of SNN
-def forward(ann, param, xv):
+def forward(ann, param, x):
   L = ann['L']
   w = ann['W']
   a = ann['a']
   z = ann['z']
 
-  a[0] = xv
+  a[0] = x
 
   for i in range(1, L+1):
     if (i == L):
+      n_fun = 5 #sigmoid
       z[i] = w[i] @ a[i-1]
-      a[i] = act_function(param['g_fun'],z[i])
+      a[i] = act_function(n_fun,z[i])
     else:
       z[i] = w[i] @ a[i-1]
       a[i] = act_function(param['g_fun'],z[i])
